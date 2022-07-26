fe2cb4393cac0bf1c7938d5e09dca1bfb3450e65
ikeman32 <david.isakson.ii@gmail.com>
2022-07-25 23:23:57 -0700

    initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='lazycode',
+    version='0.0.1',
+    description='',
+    long_description='',
+    url='https://github.com/ikeman32/lazycode-base',
+    author='me',
+    author_email='david.isakson.ii.com',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
