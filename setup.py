b7e7cf8568e15bed6fc6678d5740030105b8709f
ikeman32 <david.isakson.ii@gmail.com>
2022-07-28 16:14:30 -0700

    minor edits

--- setup.py
+++ setup.py
@@ -1,13 +1,24 @@
 from setuptools import setup
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+VERSION = '0.0.1'
 
 setup(
     name='lazycode',
-    version='0.0.1',
-    description='',
-    long_description='',
+    version=VERSION,
+    description='Lazily code common tedious code',
+    long_description='file: README.md',
     url='https://github.com/ikeman32/lazycode-base',
     author='me',
     author_email='david.isakson.ii.com',
-    packages=[],
-    classifiers=['Development Status :: 1 - Planning'],
+    packages='',
+    install_requires='none',
+    classifiers=[
+        'Development Status :: 1 - Planning/Experimental'
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'Operating System :: Platform Independent'
+    ],
 )
