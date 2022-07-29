e12e226da90c2de7d876a29e01341fef12c4eb3f
ikeman32 <david.isakson.ii@gmail.com>
2022-07-28 17:00:03 -0700

    succefull build

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 
 here = os.path.abspath(os.path.dirname(__file__))
 
-VERSION = '0.0.1'
+VERSION = '0.0.2'
 
 setup(
     name='lazycode',
@@ -11,14 +11,14 @@ setup(
     description='Lazily code common tedious code',
     long_description='file: README.md',
     url='https://github.com/ikeman32/lazycode-base',
-    author='me',
-    author_email='david.isakson.ii.com',
+    author='David H Isakson II',
+    author_email='david.isakson.ii@gmail.com',
     packages='',
     install_requires='none',
     classifiers=[
-        'Development Status :: 1 - Planning/Experimental'
-        'Intended Audience :: Developers',
-        'Programming Language :: Python :: 3',
-        'Operating System :: Platform Independent'
-    ],
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License"
+    ]
 )
